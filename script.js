71ad8a028030b49a0ec087bd515c7224e005d653
Andrew Mandrichuk <andrew.mandrichuk2007@gmail.com>
2023-08-22 18:58:21 +0200

    Refactored code into Functions and Added Text Highlighting

--- script.js
+++ script.js
@@ -5,39 +5,58 @@ const searchInput = document.querySelector("[data-search]");
 let users = [];
 
 
-fetch("https://jsonplaceholder.typicode.com/users")
-.then(res => res.json())
-.then(data => {
-  
-  users = data.map(user => {
-    const card = CardTemplate.content.cloneNode(true).children[0];
+function getUsers() {
+  fetch("https://jsonplaceholder.typicode.com/users")
+  .then(res => res.json())
+  .then(data => {
     
-    const header = card.querySelector('[data-header]');
-    const body = card.querySelector('[data-body]');
-    
-    header.textContent = user.name;
-    body.textContent = user.email;
-    
-    CardContainer.append(card);
-    
-    return { name: user.name, email: user.email, element: card };
+    users = data.map(user => {
+      const card = CardTemplate.content.cloneNode(true).children[0];
+
+      const name = card.querySelector('[data-name]');
+      const email = card.querySelector('[data-email]');
+      
+      name.textContent = user.name;
+      email.textContent = user.email;
+      
+      CardContainer.append(card);
+
+      return { name: user.name, email: user.email, element: card };
+    });
   });
-  console.log(users);
-});
+}
 
 
+function findedBySearch() {
+  searchInput.addEventListener("input", (i) => {
 
-
-searchInput.addEventListener("input", (i) => {
-  const value = i.target.value.toLowerCase();
+    // * cards show by the input text  
+    const value = i.target.value.toLowerCase();
   
-  users.forEach(user => {
-
-    const userName = user.name.toLowerCase();
-    const userEmail = user.email.toLowerCase();
-
-    const isVisible = userName.includes(value) || userEmail.includes(value);
-    
-    user.element.classList.toggle("hide", !isVisible);
+    users.forEach(user => {
+      const userName = user.name.toLowerCase();
+      const userEmail = user.email.toLowerCase();
+  
+      const isVisible = userName.includes(value) || userEmail.includes(value);
+      
+      user.element.classList.toggle("hide", !isVisible);
+  
+      // * highlight the input text
+      const cardName = user.element.querySelector('[data-name]');
+      const cardEmail = user.element.querySelector('[data-email]');
+  
+      cardName.innerHTML = highlightMatches(userName, value);
+      cardEmail.innerHTML = highlightMatches(userEmail, value);
+    });
   });
-});
+}
+
+
+function highlightMatches(text, searchValue) {
+  const re = new RegExp(searchValue, 'g');
+  return text.replace(re, match => `<span class="highlight">${match}</span>`);
+}
+
+
+getUsers();
+findedBySearch();
